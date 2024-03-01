98e2d4f096f717f9b3c3f510fdeb88dbe96d5eed
caiocroccia22 <caiocroccia22@gmail.com>
2024-03-01 00:03:38 -0300

    Adição e teste do end point da materia

---
+++ app/Http/Controllers/MateriaController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use PhpParser\Node\Expr\FuncCall;
+
+class MateriaController extends Controller
+{
+    public function index(){
+        echo "teste";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MateriaController;
 
 
 
@@ -9,9 +10,6 @@ use App\Http\Controllers\HomeController;
 Route::get('/', [HomeController::class, 'index']);
 
 
-Route::get('/{id}', function ($id) {
-    echo "materia: ".$id;
-});
-
+Route::get('/{titulo}', [MateriaController::class, 'index']);
 
 
