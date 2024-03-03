424df68c46072e57323c7e49ccb95a5a57ff12c1
caiocroccia22 <caiocroccia22@gmail.com>
2024-03-03 09:51:22 -0300

    Remoção de drivers do Microsfot SQL serve e migration pelo MySQL

--- app/Http/Controllers/Api/MateriasController.php
+++ app/Http/Controllers/Api/MateriasController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Materias;
 use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\Redis;
@@ -13,6 +14,6 @@ class MateriasController extends Controller
 {
     public function index()
     {
-        return [];
+        return Materias::all();
     }
 }
